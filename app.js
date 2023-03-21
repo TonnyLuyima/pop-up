cbe9e74c8ea1e8dec66221b45c85c220aa14dc55
Luyima-Tonny <luyimatonix@gmail.com>
2023-03-21 20:08:45 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path =require ('path');
+const router = express.Router();
+
+
+app.get('/', (req, res)=>{//new
+    res.sendFile(__dirname+ '/index.html');
+})
+
+
+
+
+
+
+
+
+app.listen(process.env.port|| 3000,()=> 
+console.log('listening on port 3000'));
